005673f82369e7213f1574cc4866ff2ccf91c0d2
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-12-11 21:34:58 +0600

    Layout design,Header, Footer done

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "../pages/shared/Header/Header";
+import Footer from "../pages/shared/Footer/Footer";
 
 const MainLayout = () => {
   return (
     <>
+      <Header />
       <Outlet></Outlet>
+      <Footer />
     </>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
